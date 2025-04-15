8ef332d7d8c31fb124da06343a58e09f7af37213
tdnikolaeva <tdnikolaeva@mail.ru>
2025-04-15 20:53:23 +0300

    fix: fix datetime import

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import datetime
+from datetime import datetime
 from mongoengine import connect, StringField, FloatField, BinaryField, DateTimeField, Document, ListField, \
     ReferenceField
 

09f2b3cde226b5010826457dccd92a117f85f926
aleksefremov <aefremov199@icloud.com>
2025-04-15 19:12:29 +0300

    feat: add more discriptions for models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from mongoengine import connect, StringField, FloatField, BinaryField, DateTimeField
+import datetime
+from mongoengine import connect, StringField, FloatField, BinaryField, DateTimeField, Document, ListField, ReferenceField
 
 load_dotenv()
 
@@ -10,8 +11,28 @@ MONGO_PORT = os.getenv('MONGO_PORT')
 
 connect(MONGO_DB_NAME, host=f"{MONGO_HOST}:{MONGO_PORT}")
 
+class Photo(Document):
+    data_photo = BinaryField()
+
+    def create_photo(cls, data_photo):
+        return cls(
+            data_photo=data_photo
+        )
+
+
+class Status(Document):
+    type = StringField(required=True)
+    timestamp = DateTimeField(default=datetime.now)
+
+    def create_status(cls, type, timestamp):
+        return cls(
+            type=type,
+            timestamp=timestamp
+        )
+
+
 # Create your models here.
-class Request():
+class Request(Document):
     title = StringField(required=True)
     description = StringField(required=True)
     address = StringField(required=True)
@@ -20,23 +41,22 @@ class Request():
     price = FloatField(required=True)
     #todo
     # добавить Photo
+    photos = ListField(ReferenceField(Photo))
+    #todo
     # добавить user_id
+
+    #todo
     # добавить statuses
+    statuses = ListField(ReferenceField(Status))
     @classmethod
-    def create_request(cls, title, description, address, price):
+    def create_request(cls, title, description, address, price, categories=None, photos=None, statuses=None):
         return cls(
             title=title,
             description=description,
             price=price,
-            address=address
+            address=address,
+            categories = categories if categories else [],
+            photos = photos if photos else [],
+            statuses = statuses if statuses else []
         )
-
-class Photo():
-    data = BinaryField()
-    pass
-
-class Status():
-    type = StringField(required=True)
-    timestamp = DateTimeField()#todo добавить дефолтное время
-    pass
 

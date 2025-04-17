752e51b95ca8637b467c58447770e083f1e973e1
R3dKar <aazz.140720045@gmail.com>
2025-04-18 01:01:59 +0300

    refactor: change code a bit

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import TestPage from '../components/pages/TestPage';
-import NotFoundPage from '../components/pages/NotFoundPage';
-
-const routes = [
-    { path: '/test', element: <TestPage/>,},
-    { path: '*', element: <NotFoundPage/>,},
-];
+import TestPage from '@src/components/pages/TestPage';
+import NotFoundPage from '@src/components/pages/NotFoundPage';
+import RegisterPage from '@src/components/pages/RegisterPage';
+import RequestsAdminPage from '@src/components/pages/RequestsAdminPage/RequestsAdminPage';
+import LoginPage from '@src/components/pages/LoginPage';
 
 const AppRouter = () => (
   <Routes>
-    {routes.map(({ path, element }) => (
-      <Route key={path} path={path} element={element} />
-    ))}
+    <Route path='login' element={<LoginPage />} />
+    <Route path='register' element={<RegisterPage />} />
+    <Route path='requests' element={<RequestsAdminPage />} />
+    <Route path='test' element={<TestPage />} />
+
+    <Route path='*' element={<NotFoundPage />} />
   </Routes>
 );
 

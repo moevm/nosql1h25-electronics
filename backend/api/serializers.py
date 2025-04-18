392bd4bb2bd46b097d75a973d39ecfe30668e1bf
dechlorine <lamashovskij.ddd@gmail.com>
2025-04-18 13:45:55 +0300

    feat: add status serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,6 +5,12 @@ class PhotoSerializer(serializers.Serializer):
     id = serializers.CharField()
     data = serializers.CharField()
 
+class StatusSerializer(serializers.Serializer):
+    type = serializers.CharField()
+    timestamp = serializers.DateTimeField()
+    price = serializers.FloatField(required=False)  # Цена только для "price_offer"
+    user_id = serializers.CharField(required=False)  # ID пользователя только для статуса с взаимодействием
+
 class RequestSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=200)
     description = serializers.CharField()
@@ -13,9 +19,13 @@ class RequestSerializer(serializers.Serializer):
         'laptop', 'smartphone', 'tablet', 'pc', 'tv', 'audio', 'console', 'periphery', 'other'
     ])
     price = serializers.FloatField()
-    photos = PhotoSerializer(many=True)
-    statuses = serializers.ListField(child=serializers.DictField())
-    user_id = serializers.CharField()
+    photos = serializers.ListField(
+        child=serializers.CharField()  # Идентификаторы фото (ObjectId в виде строки)
+    )
+    user_id = serializers.CharField()  # ID пользователя (ObjectId в виде строки)
+    statuses = serializers.ListField(
+        child=StatusSerializer()  # Вложенные объекты статусов
+    )
 
     def create(self, validated_data):
         return Request(**validated_data)

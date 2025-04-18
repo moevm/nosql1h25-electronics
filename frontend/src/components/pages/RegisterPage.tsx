fb8af798903dab36047a59c155660b85a74d3453
R3dKar <aazz.140720045@gmail.com>
2025-04-18 15:24:38 +0300

    feat: make all input controlled on register page

--- frontend/src/components/pages/RegisterPage.tsx
+++ frontend/src/components/pages/RegisterPage.tsx
@@ -1,7 +1,15 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
 import { Paper, Typography, TextField, Button, Stack, Container } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullname, setFullname] = useState('');
+  const [phone, setPhone] = useState('');
+
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
@@ -14,27 +22,56 @@ export const RegisterPage = () => {
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>Логин:</Typography>
-            <TextField placeholder='Логин' name='login' required /> 
+            <TextField
+              placeholder='Логин'
+              name='login' 
+              required
+              value={login}
+              onChange={e => setLogin(e.target.value)}
+            /> 
           </Stack> 
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>Пароль:</Typography>
-            <TextField placeholder='Пароль' type='password' name='password' required /> 
+            <TextField 
+              placeholder='Пароль' 
+              type='password' 
+              name='password' 
+              slotProps={{ htmlInput: { minLength: 8, maxLength: 50 }}} 
+              required
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            /> 
           </Stack>
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>ФИО:</Typography>
-            <TextField placeholder='Иванов Иван Иванович' name='fullname' required /> 
+            <TextField 
+              placeholder='Иванов Иван Иванович' 
+              name='fullname' 
+              slotProps={{ htmlInput: { minLength: 1, maxLength: 200 }}}  
+              required 
+              value={fullname}
+              onChange={e => setFullname(e.target.value)}
+            /> 
           </Stack>
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>Номер телефона:</Typography>
-            <TextField placeholder='+70000000000' slotProps={{ htmlInput: { pattern: '\\+7\\d{10}' } }} type='tel' name='phone' required /> 
+            <TextField 
+              placeholder='+x(xxx)xxx-xx-xx' 
+              slotProps={{ htmlInput: { pattern: '\\+\\d\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}' }}} 
+              type='tel' 
+              name='phone' 
+              required
+              value={phone}
+              onChange={e => setPhone(e.target.value)}
+            /> 
           </Stack>
 
           <Stack direction='column' gap={1}>
             <Button type='submit' variant='contained'>Зарегистрироваться</Button>
-            <Button>Авторизация</Button>
+            <Button onClick={() => navigate('/login')}>Авторизация</Button>
           </Stack>
         </Stack>
       </Paper>

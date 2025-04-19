0f93f20bbce18bf0a15447f32326f483171f4dc2
R3dKar <aazz.140720045@gmail.com>
2025-04-19 12:01:02 +0300

    fix: add login page validation

--- frontend/src/components/pages/LoginPage/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,52 +1,92 @@
-import { FormEvent, useState } from 'react';
 import { Paper, Typography, TextField, Button, Stack, Container } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { Control, Controller, useForm } from 'react-hook-form';
 import { PasswordField } from '@src/components/ui/PasswordField';
 import style from './LoginPage.module.css';
+
+interface FormInputs {
+  login: string;
+  password: string;
+}
+
+interface FormFieldProps {
+  control: Control<FormInputs>;
+}
+
+const LoginFormField = ({ control }: FormFieldProps) => (
+  <Controller
+    name='login'
+    control={control}
+    render={({ field, fieldState }) => (
+      <TextField
+        placeholder='Логин'
+        helperText={fieldState.error?.message}
+        error={!!fieldState.error}
+        value={field.value}
+        onChange={field.onChange}
+      />
+    )}
+    rules={{
+      validate: {
+        required: (value: string) => {
+          if (!value) return 'Обязательное поле';
+          if (!value.trim()) return 'Поле не может быть пустым';
+        },
+      },
+    }}
+  />
+);
+
+const PasswordFormField = ({ control }: FormFieldProps) => (
+  <Controller 
+    name='password'
+    control={control}
+    render={({ field, fieldState }) => (
+      <PasswordField 
+        placeholder='Пароль'
+        helperText={fieldState.error?.message}
+        error={!!fieldState.error}
+        value={field.value}
+        onChange={field.onChange}
+      />
+    )}
+    rules={{
+      validate: {
+        required: (value: string) => {
+          if (!value) return 'Обязательное поле';
+        },
+      },
+    }}
+  />
+);
 
 export const LoginPage = () => {
   const navigate = useNavigate();
 
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
+  const { control, handleSubmit } = useForm<FormInputs>();
 
-  const onSubmit = (e: FormEvent) => {
-    e.preventDefault();
+  const onSubmit = (data: FormInputs) => {
+    alert(JSON.stringify(data));    
   };
 
   return (
     <Container maxWidth='xs' className={style.container}>
-      <Paper 
-        component='form' 
-        elevation={5} 
-        onSubmit={onSubmit} 
-        sx={{ p: 3 }}
-      >
+      <Paper elevation={5} sx={{ p: 3 }}>
         <Stack direction='column' gap={2}>
           <Typography variant='h4'>Авторизация</Typography>
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>Логин:</Typography>
-            <TextField 
-              placeholder='Логин' 
-              name='login' 
-              value={login}
-              onChange={e => setLogin(e.target.value)}  
-            />
+            <LoginFormField control={control} />
           </Stack> 
 
           <Stack direction='column' gap={0}>
             <Typography variant='body1'>Пароль:</Typography>
-            <PasswordField 
-              placeholder='Пароль'
-              name='password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            /> 
+            <PasswordFormField control={control} /> 
           </Stack>
 
           <Stack direction='column' gap={1}>
-            <Button type='submit' variant='contained'>Войти</Button>
+            <Button variant='contained' onClick={handleSubmit(onSubmit)}>Войти</Button>
             <Button onClick={() => navigate('/register')}>Регистрация</Button>
           </Stack>
         </Stack>

f896434ec0acadc40c27f33212b7e5b1cc08da6c
tdnikolaeva <tdnikolaeva@mail.ru>
2025-04-19 12:13:25 +0300

    feat: add errors_handler with {details: message} structure

--- backend/electronics/settings.py
+++ backend/electronics/settings.py
@@ -112,6 +112,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'EXCEPTION_HANDLER': 'authapp.utils.custom_exception_handler',
 }
 
 SPECTACULAR_SETTINGS = {

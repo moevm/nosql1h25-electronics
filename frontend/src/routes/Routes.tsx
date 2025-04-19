9052bac47080e87f5717c694aee305e04f71eb35
R3dKar <aazz.140720045@gmail.com>
2025-04-19 15:37:01 +0300

    fix: remove debug route

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -11,7 +11,7 @@ const AppRouter = () => (
     <Route path='login' element={<LoginPage />} />
     <Route path='register' element={<RegisterPage />} />
     <Route path='requests' element={<RequestsAdminPage />} />
-    <Route path='requestsClient' element={<RequestsClientPage />} />
+    {/* <Route path='requests' element={<RequestsClientPage />} /> */}
     <Route path='test' element={<TestPage />} />
 
     <Route path='*' element={<NotFoundPage />} />

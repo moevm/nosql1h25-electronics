1a6cc47243a0b9474e03f5d8dd8b8df97db7a523
dZatr1k <uhuman239@gmail.com>
2025-04-22 16:30:47 +0300

    feat: change try/catch + add rubles

--- frontend/src/components/pages/ProductCardPage.tsx
+++ frontend/src/components/pages/ProductCardPage.tsx
@@ -26,25 +26,25 @@ const ProductCardPage = () => {
     if (!id) return;
 
     const loadProduct = async (id: string) => {
-      const response = await ApiService.apiRequestsRetrieve({ id });
-      console.log(response);
-      setProduct(response);
-    }
-    
-    try {
-      loadProduct(id)
-    } catch (error) {
-      setIsError(true);
-      if (error instanceof AxiosError && error.response) {
-        setErrorProps({
-          title: `${error.response.status}: ${error.response.statusText}`,
-          message: error.message
-        });
+      try {
+        const response = await ApiService.apiRequestsRetrieve({ id });
+        setProduct(response);
+      } catch (error) {
+        setIsError(true);
+        if (error instanceof AxiosError && error.response) {
+          setErrorProps({
+            title: `${error.response.status}: ${error.response.statusText}`,
+            message: error.message
+          });
+        }
       }
     }
+
+    loadProduct(id)
+
   }, [id]);
 
-  if(isError){
+  if (isError) {
     return <ErrorMessage {...(errorProps || {})} />;
   }
 
@@ -74,7 +74,7 @@ const ProductCardPage = () => {
             <Typography variant="h5" gutterBottom>{product.title}</Typography>
             <Typography variant="body1" component="p" >{product.description}</Typography>
             <Typography variant="body2" gutterBottom><strong>Категория:</strong> {categoryToRussian(product.category)}</Typography>
-            <Typography variant="body2" gutterBottom><strong>Цена:</strong> {product.price}</Typography>
+            <Typography variant="body2" gutterBottom><strong>Цена:</strong> {product.price} ₽</Typography>
             <Typography variant="body2" gutterBottom><strong>Адрес:</strong> {product.address}</Typography>
 
             <Button variant="contained" color="error" sx={{ mt: 3 }}>

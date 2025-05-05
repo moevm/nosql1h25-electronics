9bc5450eb9594c8ec0e583d7e774c8cc67ff0b90
dZatr1k <uhuman239@gmail.com>
2025-05-05 22:22:39 +0300

    feat: remove localization provider form DateInput

--- frontend/src/components/ui/timeline/inputs/DateInput.tsx
+++ frontend/src/components/ui/timeline/inputs/DateInput.tsx
@@ -1,15 +1,13 @@
 import { FiberManualRecord, CheckCircle } from "@mui/icons-material";
 import { Box, Typography, TextField, IconButton, Input } from "@mui/material";
-import { DateTimePicker, LocalizationProvider, MobileDateTimePicker } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MobileDateTimePicker } from "@mui/x-date-pickers";
 import { ApiService } from "@src/api";
 import { useAppSelector } from "@src/hooks/ReduxHooks";
 import { selectUser } from "@src/store/UserSlice";
 import dayjs from "dayjs";
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import 'dayjs/locale/ru';
-import { ruRU } from "@mui/x-date-pickers/locales";
 
 dayjs.locale('ru');
 
@@ -40,10 +38,8 @@ const DateInput = ({ requestId }: { requestId: string }) => {
     <Box sx={{ display: 'flex', alignItems: 'center', mt:1 }}>
       <FiberManualRecord sx={{ fontSize: 8, mr: 1 }} />
       <Typography sx={{ pr: 1 }}>Предложить <strong>дату</strong>:</Typography>
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru" localeText={ruRU.components.MuiLocalizationProvider.defaultProps.localeText}>
-        <MobileDateTimePicker disablePast orientation="landscape" value={date} ampm={false} minutesStep={5}
+      <MobileDateTimePicker disablePast orientation="landscape" value={date} ampm={false} minutesStep={5}
           format="DD.MM.YYYY HH:mm" onChange={(newValue) => setDate(newValue)} />
-      </LocalizationProvider>
 
       <IconButton disabled={date == null} color="success" onClick={() => handleDate()} edge="end">
         <CheckCircle />

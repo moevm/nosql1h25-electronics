389ac6b228de284c608291f04c4a82c58476cd37
dZatr1k <uhuman239@gmail.com>
2025-05-20 16:34:40 +0300

    fix: change admin route

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -7,11 +7,12 @@ import { AuthorizedOnly } from './AuthorizedOnly';
 import { EditProfilePage } from '@src/components/pages/EditProfilePage';
 import { ProductsPage } from '@src/components/pages/ProductsPage';
 import StatisticsPage from '@src/components/pages/StatisticsPage';
+import AdminOnly from './AdminOnly';
 
 const routes = [
   { path: '/products', element: <AuthorizedOnly><ProductsPage /></AuthorizedOnly> },
   { path: '/product/:id', element: <AuthorizedOnly><ProductCardPage /></AuthorizedOnly> },
-  { path: '/statistics', element: <AuthorizedOnly><StatisticsPage /></AuthorizedOnly> },
+  { path: '/statistics', element: <AdminOnly><StatisticsPage /></AdminOnly> },
   { path: '/profile', element: <AuthorizedOnly><EditProfilePage /></AuthorizedOnly> },
   { path: '/login', element: <AuthorizedOnly inverted><LoginPage /></AuthorizedOnly>},
   { path: '/register', element: <AuthorizedOnly inverted><RegisterPage /></AuthorizedOnly>},
